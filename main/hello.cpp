f098d984cfc206696386dce0fd014f47d43e9c17
Adithya Ganesh <97718839+adithyag17@users.noreply.github.com>
2024-03-06 13:58:51 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "web hook added successfully!\n";
+  cout << "web hook added successfully\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
